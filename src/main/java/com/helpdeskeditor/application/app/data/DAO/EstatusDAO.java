fabb497ab3e831b46e56ab56d924aa266b7c30a3
Usuario <davideocho@gmail.com>
2023-01-10 05:14:43 -0600

    DAO estatus

--- src/main/java/com/helpdeskeditor/application/app/data/DAO/EstatusDAO.java
+++ src/main/java/com/helpdeskeditor/application/app/data/DAO/EstatusDAO.java
@@ -4,9 +4,40 @@ import com.helpdeskeditor.application.app.data.entity.EstatusEntity;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Date;
+
 @Getter
 @Setter
-public class EstatusDAO extends EstatusEntity {
-    String Nombre;
-    String NombrePropio;
+public class EstatusDAO{
+
+    Integer id;
+
+    Integer folio;
+
+    Integer idEstatus;
+
+    String nombre;
+
+    String anotacion;
+
+    Integer idUsuario;
+
+    String nombrePropio;
+
+    Date fecha;
+
+    public EstatusDAO(Integer id, Integer folio, Integer idEstatus, String nombre, String anotacion, Integer idUsuario, String nombrePropio, Date fecha) {
+        this.id = id;
+        this.folio = folio;
+        this.idEstatus = idEstatus;
+        this.nombre = nombre;
+        this.anotacion = anotacion;
+        this.idUsuario = idUsuario;
+        this.nombrePropio = nombrePropio;
+        this.fecha = fecha;
+    }
 }
