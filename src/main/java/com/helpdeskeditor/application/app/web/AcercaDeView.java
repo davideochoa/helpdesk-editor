f0432ff46686289cc75ab13782b91a18ca8cd673
David E. Ochoa Diaz <57770708+davideochoa@users.noreply.github.com>
2023-03-28 04:48:08 -0600

    Pad firma

--- src/main/java/com/helpdeskeditor/application/app/web/AcercaDeView.java
+++ src/main/java/com/helpdeskeditor/application/app/web/AcercaDeView.java
@@ -1,6 +1,7 @@
 package com.helpdeskeditor.application.app.web;
 
 import com.helpdeskeditor.application.app.web.MainLayout;
+import com.vaadin.flow.component.formlayout.FormLayout;
 import com.vaadin.flow.component.html.H2;
 import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.html.Paragraph;
@@ -8,6 +9,7 @@ import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.server.auth.AnonymousAllowed;
+import de.f0rce.signaturepad.SignaturePad;
 import lombok.extern.slf4j.Slf4j;
 
 import javax.annotation.security.RolesAllowed;
@@ -30,6 +32,23 @@ public class AcercaDeView extends VerticalLayout {
 
         add(new H2("This place intentionally left empty"));
         add(new Paragraph("It’s a place where you can grow your own UI 🤗"));
+
+
+        SignaturePad signature = new SignaturePad();
+        signature.setClearButtonVisible(true);
+        signature.setHeight("150px");
+        signature.setWidth("300px");
+        signature.setBackgroundColor(0, 0, 0, 0);
+        signature.setPenColor("#000000");
+        signature.setVisible(true);
+
+        add(new H2("This place intentionally left empty"));
+
+        FormLayout formLayout = new FormLayout();
+        formLayout.add(signature);
+        add(formLayout);
+        add(new H2("This place intentionally left empty"));
+
 
         setSizeFull();
         setJustifyContentMode(JustifyContentMode.CENTER);
