1637208826a159a28ba90e177b5ea9626fd9ece3
David Ochoa <davideochoa@gmail.com>
2023-06-14 05:03:07 -0600

    Release Final v7 camera

--- src/main/java/com/helpdeskeditor/application/Application.java
+++ src/main/java/com/helpdeskeditor/application/Application.java
@@ -20,6 +20,7 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 @Theme(value = "helpdeskeditor")
 @NpmPackage(value = "line-awesome", version = "1.3.0")
 @NpmPackage(value = "@vaadin-component-factory/vcf-nav", version = "1.0.6")
+@PWA(name = "", shortName = "")
 public class Application implements AppShellConfigurator {
 
     public static void main(String[] args) {
