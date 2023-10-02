09ca667b25bc540dafcde33c0e4b23503ad229ae
David Ochoa <davideochoa@gmail.com>
2023-10-02 05:51:36 -0600

    dashboard 2

--- src/main/java/com/helpdeskeditor/application/app/web/DashBoard2.java
+++ src/main/java/com/helpdeskeditor/application/app/web/DashBoard2.java
@@ -71,7 +71,7 @@ public class DashBoard2 extends VerticalLayout {
             Date inicio2 = Date.from(DP_fechaInicio.getValue().atStartOfDay().atZone(ZoneId.systemDefault()).toInstant());
             Date fin2 = Date.from(DP_fechaFin.getValue().atStartOfDay().atZone(ZoneId.systemDefault()).toInstant());
 
-            dash[0] = new GraficaPastelFoliosXUnidad(folioService, inicio2, fin2);
+            dash[0] = new GraficaLineaFoliosGenerados(folioService, inicio2, fin2);
 
             flgraficas.setColspan(dash[0], 3);
 
