ce72aff1839be29d0f996a6ced62f9b423ad3827
David Ochoa <davideochoa@gmail.com>
2023-10-04 04:31:53 -0600

    dashboard 2

--- src/main/java/com/helpdeskeditor/application/app/web/DashBoard1.java
+++ src/main/java/com/helpdeskeditor/application/app/web/DashBoard1.java
@@ -79,10 +79,10 @@ public class DashBoard1 extends VerticalLayout {
             Date inicio2 = Date.from(DP_fechaInicio.getValue().atStartOfDay().atZone(ZoneId.systemDefault()).toInstant());
             Date fin2 = Date.from(DP_fechaFin.getValue().atStartOfDay().atZone(ZoneId.systemDefault()).toInstant());
 
-            dash[0] = new GraficaPastelFoliosXUnidad(folioService, inicio, fin);
-            dash[1] = new GraficaPastelFoliosXIncidecia(folioService, inicio, fin);
-            dash[2] = new GraficasIntegralesXUnidad(folioService, inicio, fin);
-            dash[3] = new GraficasIntegralesXBien(folioService, inicio, fin);
+            dash[0] = new GraficaPastelFoliosXUnidad(folioService, inicio2, fin2);
+            dash[1] = new GraficaPastelFoliosXIncidecia(folioService, inicio2, fin2);
+            dash[2] = new GraficasIntegralesXUnidad(folioService, inicio2, fin2);
+            dash[3] = new GraficasIntegralesXBien(folioService, inicio2, fin2);
 
             flgraficas.setColspan(dash[0], 3);
             flgraficas.setColspan(dash[1], 3);
