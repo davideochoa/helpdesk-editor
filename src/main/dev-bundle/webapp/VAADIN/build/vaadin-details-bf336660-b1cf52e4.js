d557a8d8492dc8119e629cae38bb4a4c6d9743f5
David Ochoa <davideochoa@gmail.com>
2023-11-03 20:47:18 -0600

    modifica mostrar reporte

---
+++ src/main/dev-bundle/webapp/VAADIN/build/vaadin-details-bf336660-b1cf52e4.js
@@ -0,0 +1,1 @@
+import{y as e}from"./indexhtml-90f80f1e.js";const t={tagName:"vaadin-details",displayName:"Details",elements:[{selector:"vaadin-details",displayName:"Root element",properties:e}]};export{t as default};
---
+++ src/main/dev-bundle/webapp/VAADIN/build/vaadin-spreadsheet-59d8c5ef-2d89fd8e.js
@@ -0,0 +1,1 @@
+import{I as e}from"./indexhtml-90f80f1e.js";const r={tagName:"vaadin-spreadsheet",displayName:"Spreadsheet",elements:[{selector:"vaadin-spreadsheet",displayName:"Spreadsheet",properties:[e.backgroundColor,e.borderColor,e.borderWidth,e.borderRadius,e.padding]}]};export{r as default};
